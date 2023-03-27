6de7eb92972f2948d1982a936c1c55fdf4303a0e
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-03-27 09:37:55 -0400

    [Publishing] Add github actions for publishing (#6)
    Setup Github for publishing and signing via actions.
    
    Closes #4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import Versions.configureJavaToolchain
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+import java.net.URI
 
 plugins {
   kotlin("jvm") version Versions.KOTLIN
@@ -8,6 +10,7 @@ plugins {
   id("com.jaredsburrows.license") version "0.9.0"
   id("com.autonomousapps.dependency-analysis") version "1.19.0"
   `maven-publish`
+  signing
 }
 
 buildscript {
@@ -35,8 +38,22 @@ apply {
   plugin("maven-publish")
 }
 
-group = "com.github.nava2.kinterval-tree"
-version = "0.1.0"
+group = "net.navatwo"
+archivesName.set("kinterval-tree")
+version = "0.1.0-SNAPSHOT"
+
+if (System.getenv("CI") == "true") {
+  when (val eventName = System.getenv("GITHUB_EVENT_NAME")) {
+    "release" -> {
+      version = version.toString().substringBefore("-SNAPSHOT")
+      logger.info("Deploying version: $version")
+    }
+    "push", "pull_request" -> Unit
+    else -> {
+      logger.warn("unknown event: $eventName")
+    }
+  }
+}
 
 repositories {
   mavenCentral()
@@ -52,8 +69,51 @@ dependencies {
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
+val javadocJar by tasks.register<Jar>("javadocJar") {
+  archiveClassifier.set("javadoc")
+  from(tasks.named("javadoc"))
+}
+
+val sourcesJar by tasks.register<Jar>("sourcesJar") {
+  archiveClassifier.set("sources")
+  from(sourceSets.main.get().allSource)
+}
+
+artifacts {
+  archives(sourcesJar)
+  archives(javadocJar)
+}
+
+signing {
+  val signingKeyId: String? by project
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+  sign(configurations.archives.get())
+}
+
 publishing {
   publications {
+    repositories {
+      maven {
+        repositories {
+          maven {
+            name = "ossrh"
+
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+            credentials {
+              username = System.getenv("OSSRH_USERNAME")
+              password = System.getenv("OSSRH_PASSWORD")
+            }
+          }
+        }
+      }
+    }
+
     create<MavenPublication>("maven") {
       from(components["kotlin"])
       pom {

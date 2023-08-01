35a7258262c27f75b7fa54e528364628e76f1262
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-07-31 21:17:43 -0400

    [Build] Enable build and configuration caches (#33)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "kinterval-tree"
+
+plugins {
+  id("com.gradle.enterprise") version "3.14.1"
+}
+
+gradleEnterprise {
+  buildScan {
+    publishAlways()
+
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
